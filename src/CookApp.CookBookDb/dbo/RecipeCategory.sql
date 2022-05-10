4d952d353b9417d024ace43f23feb215da5089e2
Alexander Gribowski <grias1994@gmail.com>
2022-05-10 13:53:47 +0300

    Add db project with Recipe, Category and RecipeCategory tables

---
+++ src/CookApp.CookBookDb/dbo/RecipeCategory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[RecipeCategory]
+(
+	[RecipeId] INT NOT NULL,
+	[CategoryId] INT NOT NULL,
+	CONSTRAINT [FK_RecipeCategory_Recipe] FOREIGN KEY ([RecipeId]) REFERENCES [dbo].[Recipe] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_RecipeCategory_Category] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [PK_RecipeCategory] PRIMARY KEY ([RecipeId], [CategoryId])
+)
