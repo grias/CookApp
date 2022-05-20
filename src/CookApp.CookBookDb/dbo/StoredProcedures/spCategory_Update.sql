3987fa6545f1a8555557f2f5f8fa3b600205d9af
Alexander Gribowski <grias1994@gmail.com>
2022-05-20 16:18:19 +0300

    fix spCategory_Update to have proper parameters and updatet table

--- src/CookApp.CookBookDb/dbo/StoredProcedures/spCategory_Update.sql
+++ src/CookApp.CookBookDb/dbo/StoredProcedures/spCategory_Update.sql
@@ -1,9 +1,10 @@
 ﻿CREATE PROCEDURE [dbo].[spCategory_Update]
 	@CategoryId INT,
-	@Name NVARCHAR(MAX)
+	@Name NVARCHAR(MAX),
+	@Description NVARCHAR(MAX)
 AS
 BEGIN
-	UPDATE [dbo].[Recipe] 
-	SET [Name] = @Name
+	UPDATE [dbo].[Category] 
+	SET [Name] = @Name, [Description] = @Description
 	WHERE [Id] = @CategoryId
 END
