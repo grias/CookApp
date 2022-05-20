5be76459abaa29c79247a5d3e28091cbcf5bdfa7
Alexander Gribowski <grias1994@gmail.com>
2022-05-20 17:19:32 +0300

    add more tests to CategoryRepository

--- tests/Infrastructure.IntegrationTests/CategoryRepositoryTests.cs
+++ tests/Infrastructure.IntegrationTests/CategoryRepositoryTests.cs
@@ -27,9 +27,9 @@ public class CategoryRepositoryTests
     private readonly List<Category> _fakeCategories = new List<Category>()
     {
         new Category(-1, "Cakes", "Tasty cakes"),
-        new Category(-1, "Salads", "Tasty salads"),
-        new Category(-1, "Cocktails", "Tasty cocktails"),
-        new Category(-1, "Soups", "Tasty soups"),
+        new Category(-1, "Eggs", "Tasty eggs"),
+        new Category(-1, "Ales", "Tasty ales"),
+        new Category(-1, "Ducks", "Tasty ducks"),
         new Category(-1, "Burgers", "Tasty burgers")
     };
 
@@ -129,5 +129,165 @@ public class CategoryRepositoryTests
 
         // Assert
         Assert.Equal(_fakeCategories.Count, categories.Count);
+    }
+
+    [Fact]
+    public async void GetPageAsync_SecondPage_ReturnsCorrectCategories()
+    {
+        // Arrange
+        var categoryRepository = new CategoryRepository(_connectionString);
+        await ClearCategoryTable();
+        await PopulateCategoryTableByFakeData();
+        var sortedFakeCategories = _fakeCategories.OrderBy(c => c.Name).ToList();
+        int pageNumber = 2;
+        int pageSize = 2;
+        var secondPage = new Pagination(pageNumber, pageSize);
+
+        // Act
+        var categories = (await categoryRepository.GetPageAsync(secondPage)).ToList();
+
+        // Assert
+        Assert.Equal(2, categories.Count);
+        Assert.Equal(sortedFakeCategories[2].Name, categories[0].Name);
+        Assert.Equal(sortedFakeCategories[3].Name, categories[1].Name);
+    }
+
+    [Fact]
+    public async void GetPageAsync_NonexistentPage_ReturnsEmptyCollection()
+    {
+        // Arrange
+        var categoryRepository = new CategoryRepository(_connectionString);
+        await ClearCategoryTable();
+        await PopulateCategoryTableByFakeData();
+
+        // Act
+        var categories = (await categoryRepository.GetPageAsync(new Pagination(int.MaxValue, 1))).ToList();
+
+        // Assert
+        Assert.Empty(categories);
+    }
+
+    [Fact]
+    public async void InsertAsync_newCategory_ReturnsInsertedCategory()
+    {
+        // Arrange
+        var categoryRepository = new CategoryRepository(_connectionString);
+        await ClearCategoryTable();
+
+        // Act
+        var category = new NewCategory("CorrectName", "CorrectDescription");
+        var insertedCategory = await categoryRepository.InsertAsync(category);
+
+        // Assert
+        Assert.Equal(category.Name, insertedCategory.Name);
+        Assert.Equal(category.Description, insertedCategory.Description);
+    }
+
+    [Fact]
+    public async void InsertAsync_newCategory_InsertsCorrectCategory()
+    {
+        // Arrange
+        var categoryRepository = new CategoryRepository(_connectionString);
+        await ClearCategoryTable();
+
+        // Act
+        var category = new NewCategory("CorrectName", "CorrectDescription");
+        await categoryRepository.InsertAsync(category);
+
+        // Assert
+        
+        using var db = new SqlConnection(_connectionString);
+        var insertedCategory = await db.QuerySingleOrDefaultAsync<Category>(
+            "SELECT * FROM Category WHERE Name = @Name AND Description = @Description",
+            new { category.Name, category.Description });
+        Assert.NotNull(insertedCategory);
+    }
+
+    [Fact]
+    public async void InsertAsync_ExistingName_ThrowsSqlException()
+    {
+        // Arrange
+        var categoryRepository = new CategoryRepository(_connectionString);
+        await ClearCategoryTable();
+        await PopulateCategoryTableByFakeData();
+
+        // Act
+        var category = new NewCategory(_fakeCategories[0].Name, "CorrectDescription");
+        var action = async () => await categoryRepository.InsertAsync(category);
+
+        // Assert
+        await Assert.ThrowsAsync<SqlException>(action);
+    }
+
+    [Fact]
+    public async void UpdateAsync_ExistingCategory_UpdatesCategory()
+    {
+        // Arrange
+        var categoryRepository = new CategoryRepository(_connectionString);
+        await ClearCategoryTable();
+        var ids = await PopulateCategoryTableByFakeData();
+
+        // Act
+        var categoryUpdate = new Category(ids[0], "CorrectName", "CorrectDescription");
+        await categoryRepository.UpdateAsync(categoryUpdate);
+
+        // Assert
+        using var db = new SqlConnection(_connectionString);
+        var updatedCategory = await db.QuerySingleOrDefaultAsync<Category>(
+            "SELECT * FROM Category WHERE Id = @Id", new { Id = ids[0] });
+
+        Assert.Equal(categoryUpdate.Id, updatedCategory.Id);
+        Assert.Equal(categoryUpdate.Name, updatedCategory.Name);
+        Assert.Equal(categoryUpdate.Description, updatedCategory.Description);
+    }
+
+    [Fact]
+    public async void UpdateAsync_ExistingCategory_ReturnsUpdatedCategory()
+    {
+        // Arrange
+        var categoryRepository = new CategoryRepository(_connectionString);
+        await ClearCategoryTable();
+        var ids = await PopulateCategoryTableByFakeData();
+
+        // Act
+        var categoryUpdate = new Category(ids[0], "CorrectName", "CorrectDescription");
+        var updatedCategory = await categoryRepository.UpdateAsync(categoryUpdate);
+
+        // Assert
+        Assert.Equal(categoryUpdate.Id, updatedCategory.Id);
+        Assert.Equal(categoryUpdate.Name, updatedCategory.Name);
+        Assert.Equal(categoryUpdate.Description, updatedCategory.Description);
+    }
+
+    [Fact]
+    public async void UpdateAsync_NonExistingCategory_ReturnsNull()
+    {
+        // Arrange
+        var categoryRepository = new CategoryRepository(_connectionString);
+        await ClearCategoryTable();
+        var ids = await PopulateCategoryTableByFakeData();
+
+        // Act
+        var categoryUpdate = new Category(ids.Last() + 1, "CorrectName", "CorrectDescription");
+        var updatedCategory = await categoryRepository.UpdateAsync(categoryUpdate);
+
+        // Assert
+        Assert.Null(updatedCategory);
+    }
+
+    [Fact]
+    public async void UpdateAsync_ExistingName_ThrowsSqlException()
+    {
+        // Arrange
+        var categoryRepository = new CategoryRepository(_connectionString);
+        await ClearCategoryTable();
+        var ids = await PopulateCategoryTableByFakeData();
+
+        // Act
+        var categoryUpdate = new Category(ids[0], _fakeCategories[1].Name, "CorrectDescription");
+        var action = async () => await categoryRepository.UpdateAsync(categoryUpdate);
+
+        // Assert
+        await Assert.ThrowsAsync<SqlException>(action);
     }
 }
