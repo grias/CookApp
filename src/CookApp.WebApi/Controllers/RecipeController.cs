ce84b2452a6c177223234d5d0d46efc6dc67a272
Alexander Gribowski <grias1994@gmail.com>
2022-05-30 18:02:41 +0300

    add RecipeController

---
+++ src/CookApp.WebApi/Controllers/RecipeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CookApp.Domain.Core;
+using CookApp.Domain.Models;
+using CookApp.Domain.Models.CreatedEntities;
+using CookApp.Domain.Models.DbEntities;
+using CookApp.Domain.UtilityClasses;
+
+
+namespace CookApp.WebApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class RecipeController : ControllerBase
+{
+    private readonly IRecipeCollection _recipes;
+
+    public RecipeController(IRecipeCollection recipes)
+    {
+        _recipes = recipes;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<BriefRecipeWithBriefCategories>))]
+    public async Task<ActionResult<List<BriefRecipeWithBriefCategories>>> GetPageBrief([FromQuery] int page = 1, [FromQuery] int page_size = 10)
+    {
+        var recipes = await _recipes.GetBriefRecipesWithBriefCategoriesAsync(new Pagination(page, page_size));
+        return Ok(recipes.ToList());
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(RecipeWithBriefCategories))]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Recipe>> GetRecipe(int id)
+    {
+        var recipe = await _recipes.GetRecipeWithCategoriesAsync(id);
+        if (recipe == null)
+        {
+            return NotFound();
+        }
+        return Ok(recipe);
+    }
+}
--- src/CookApp.WebApi/Program.cs
+++ src/CookApp.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using CookApp.Domain.Core;
+
 namespace CookApp.WebApi
 {
     public class Program
@@ -12,6 +14,8 @@ namespace CookApp.WebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddTransient<IRecipeCollection, RecipeCollection>();
 
             var app = builder.Build();
 
